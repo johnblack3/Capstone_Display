f104980547ccc3f80c720892ae36e81068207a33
KushalRanp <kar6255@psu.edu>
2024-02-27 11:21:52 -0500

    fixing constant window issue

--- imgscript.js
+++ imgscript.js
@@ -1,11 +1,16 @@
 function showpopup(content)
 {
-    document.getElementById('popup').innerHTML = content;
+    document.getElementById('popup-title').innerHTML = "Popup";
+    document.getElementById('popup-content').innerHTML = content;
     document.getElementById('popup').style.display = "block";
     document.getElementById('popup').classList.remove("hidden");
 }
 
 function hidepopup()
 {
-    document.getElementById('popup').classList.add("hidden");
+    console.log("Hiding popup");
+    document.getElementById('popup').style.display = "none";
 }
+
+var content1 = "<h1>Popup</h1><p>This is a popup</p>";
+var content2 = "<h1>Popup</h1><p>This is another popup</p>";
